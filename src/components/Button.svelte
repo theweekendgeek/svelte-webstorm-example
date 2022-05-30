90f1eb24ce125442971f683ecbd15bc4a8eca056
Marcus Deh <deh.marcus@outlook.de>
2022-05-30 21:40:12 +0200

    feat(button): basic button component

---
+++ src/components/Button.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  export let title = '';
+  export let handleClick: CallableFunction;
+
+  const onClick = () => {
+    if (!handleClick) {
+      console.warn(`no handleClick Method passed to button with title ${title}`);
+      return;
+    }
+
+    handleClick();
+  };
+</script>
+
+<button on:click|preventDefault={onClick}> {title} </button>
+
+<style lang="scss">
+  @import '../styles/colors.scss';
+
+  button {
+    background: $secondary;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,7 @@
+<script>
+  import Button from '../components/Button.svelte';
+</script>
+
 <h1>Jounrnal</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Button title="Test" />
